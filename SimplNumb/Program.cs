4b7836add644eda2f5895b9435a378b204a71760
BelarusVadzim <BelarusVadzim@gmail.com>
2017-04-07 01:52:32 +0300

    opt

--- SimplNumb/Program.cs
+++ SimplNumb/Program.cs
@@ -14,11 +14,7 @@ namespace PrimeNumb
             DateTime Start, Fin;
             
             TimeSpan TSpan;
-            //ar = S.GetPrimeNumbers(2, 40);
-            //for (int i = 0; i < ar.Length; i++)
-            //{
-            //    Console.WriteLine(ar[i].ToString());
-            //}
+
             string inputString = null;
             //bool result = false;
 
@@ -26,27 +22,19 @@ namespace PrimeNumb
             {
                 Start = DateTime.Now;
                 PrimeNumbersList.Clear();
-                //    if (inputString == "x") break;
-                //    result = S.ChekPrimeNumber(long.Parse(inputString));
-                //    Console.WriteLine(result.ToString());
-                //}
+     
 
-                ar = S.GetPrimeNumbersN(10, 200000);
+                ar = S.GetPrimeNumbersN(1999998, 2000000);
                 foreach (long a in ar)
                 {
                     if (S.ChekPrimeNumber(a)) PrimeNumbersList.Add(a);
-                   // if (S.ChekPrimeNumber(a)) Console.WriteLine(a.ToString());
                 }
-                //foreach (long a in PrimeNumbersList)
-                //{
-                //    Console.WriteLine(a.ToString());
-                //}
+        
                 Fin = DateTime.Now;
                 TSpan = DateTime.Now - Start;
                 Console.WriteLine("--------------------------------------");
-                Console.WriteLine(Math.Round(TSpan.TotalMilliseconds) + "мс");
+                Console.WriteLine(Math.Round(TSpan.TotalMilliseconds) + " мс");
                 inputString = Console.ReadLine();
-                //Console.ReadLine();
             }
         }
     }
